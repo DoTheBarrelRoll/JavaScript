a85285cd53a9535b5421a12c362e051b8301da19
DoTheBarrelRoll <32955643+DoTheBarrelRoll@users.noreply.github.com>
2018-11-20 19:43:26 +0200

    Lisäilty juttuja

--- Finnkino API/koodi.js
+++ Finnkino API/koodi.js
@@ -1,9 +1,9 @@
 function getMovies() {
 
   var raaka = document.getElementById("pvm").value;
-  var aika = raaka.slice(8,10) + "." + raaka.slice(5,7) + "." + raaka.slice(0,4);
+  var aika = raaka.slice(8, 10) + "." + raaka.slice(5, 7) + "." + raaka.slice(0, 4);
   var teatteri = document.getElementById("teatteri").value;
-  var url = "https://www.finnkino.fi/xml/Schedule/?area="+ teatteri +"&dt=" + aika;
+  var url = "https://www.finnkino.fi/xml/Schedule/?area=" + teatteri + "&dt=" + aika;
 
 
   xmlhttp = new XMLHttpRequest();
@@ -11,8 +11,6 @@ function getMovies() {
   xmlhttp.send()
 
   xmlhttp.onreadystatechange = function() {
-    console.log(xmlhttp.readyState);
-    console.log(xmlhttp.status);
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
       var xml = xmlhttp.responseXML;
       console.log(xml);
@@ -25,19 +23,26 @@ function getMovies() {
 
       document.getElementById("elokuvat").innerHTML = "";
 
-      for (var i = 0; i < nimet.length; i++) {
-        var aika = ajat[i].innerHTML.slice(11, 16);
-        document.getElementById("elokuvat").innerHTML += `<div class="card">
-                                                            <img class="card-img-top" src="`+ kuvat[i].innerHTML + `">
-                                                            <div class="card-body">
-                                                              <h5 class="card-title">` + nimet[i].innerHTML + `</h5>
-                                                              <h6 class="card-subtitle mb-2 text-muted"> <img class="icon" src="clock.png">`+ aika + `</h6>
-                                                              <h6 class="card-subtitle mb-2 text-muted">`+ teatterit[i].innerHTML+ `</h6>
-                                                              <a href="` + esitysLinkit[i].innerHTML + `" class="card-link">Osta liput</a>
-                                                              <a href="` + tietoLinkit[i].innerHTML + `" class="card-link">Elokuvan tiedot</a>
-                                                            </div>
-                                                          </div>`
+      if (nimet.length > 0) {
+        for (var i = 0; i < nimet.length; i++) {
+          var aika = ajat[i].innerHTML.slice(11, 16);
+          document.getElementById("elokuvat").innerHTML += `<div class="card">
+                                                              <img class="card-img-top" src="` + kuvat[i].innerHTML + `">
+                                                              <div class="card-body">
+                                                                <h5 class="card-title">` + nimet[i].innerHTML + `</h5>
+                                                                <h6 class="card-subtitle mb-2 text-muted"> <img class="icon" src="clock.png">` + aika + `</h6>
+                                                                <h6 class="card-subtitle mb-2 text-muted">` + teatterit[i].innerHTML + `</h6>
+                                                                <a href="` + esitysLinkit[i].innerHTML + `" class="card-link">Osta liput</a>
+                                                                <a href="` + tietoLinkit[i].innerHTML + `" class="card-link">Elokuvan tiedot</a>
+                                                              </div>
+                                                            </div>`
+
+        }
+        document.getElementById("eikuvia").innerHTML = "";
+      } else {
+        document.getElementById("eikuvia").innerHTML = "<h2>Elokuvia ei löytynyt, kokeile toista päivää tai teatteria</h2>";
       }
+
 
     }
   }
@@ -45,6 +50,6 @@ function getMovies() {
 
 function test() {
   var pvm = document.getElementById("pvm").value;
-  var muotoiltu = pvm.slice(8,10) + "." + pvm.slice(5,7) + "." + pvm.slice(0,4);
+  var muotoiltu = pvm.slice(8, 10) + "." + pvm.slice(5, 7) + "." + pvm.slice(0, 4);
   alert(muotoiltu);
 }
